c62de314e42539c8f3313e7a4b7675a075768313
Vaidas <vaidas.kliukinskas@gmail.com>
2023-06-19 22:03:24 +0300

    initial comit

---
+++ mokymaipython/main.py
@@ -0,0 +1,29 @@
+
+
+
+
+# {
+#     "duomenuvardas": "jonas",
+#     "metai": 15,
+#     "adresas": "veiveru g. 15, neveronys",
+# },
+
+
+
+# "pets":[
+#     {
+#         "vardas": "reksas",
+#         "rusis": "poliarinemeska",
+# "metai": 5,
+#     },
+# {
+#     "vardas": "reksas",
+#     "rusis": "labradoras",
+# "metai": 4
+# }
+# ]
+
+{
+    "fruits":["bananas", "obuolys", "kivis"]
+
+}
